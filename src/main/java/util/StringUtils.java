30a21a797f501ddf8b21eb28c46b21322fbf4f1b
chitpc <wangweiwei@changhongit.com>
2016-09-08 15:02:06 +0800

    pom update

--- src/main/java/util/StringUtils.java
+++ src/main/java/util/StringUtils.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.Test;
 
 public class StringUtils {
@@ -12,7 +15,21 @@ public class StringUtils {
 	
 	@Test
 	public void testGetERPUserInfo(){
-		
+		String str = "http://172.28.4.224:8099/ClientCreditSystem/photo/2010/7/10/11453_page-0032.jpg";
+		String substring = str.substring(str.lastIndexOf("/"));
+		System.out.println(substring);
 	}
 
+	@Test
+	public void testGetERPUrl(){
+		String str = "http://172.28.4.224:8099/ClientCreditSystem/photo/2010/7/10/11453_page-0032.jpg";
+		try {
+			URL url = new URL(str);
+			String content = url.getFile();
+			System.out.println(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
